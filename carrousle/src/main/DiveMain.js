c0727c6579d4587d29b9736b1e8e63f03c7df4b0
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-21 13:36:43 -0800

    feat (divMain) : add animation to div

--- carrousle/src/main/DiveMain.js
+++ carrousle/src/main/DiveMain.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './divMain.css';
 class Divemain extends Component {
+    
     render() {
+        const  allDiv=document.querySelectorAll('.caroussel')
+        let i=0
+        let j= allDiv.length-1
         return (
             <div>
                 <h4>What People say about Abderrahmane ETTOUNANI</h4>
                 <section>
+                    
+                    {
+                        setInterval(() => {
+                            
+                            allDiv[i].setAttribute('className','first');
+                            allDiv[j].setAttribute('className','last');
+                            i++;
+                            j--;
+                        }, 2000)
+                    }
+                    
                     <div className='caroussel'></div>
                     <div className='caroussel'></div>
                     <div className='caroussel'></div>
