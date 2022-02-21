25d6c16c4e245ab34b3b18b937d2b3510a1a5a01
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-21 13:02:36 -0800

    feat (DivMain) : add style to div

--- carrousle/src/main/DiveMain.js
+++ carrousle/src/main/DiveMain.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-
+import './divMain.css';
 class Divemain extends Component {
     render() {
         return (
             <div>
-                <h1>hello from DivMain</h1>
+                <h4>What People say about Abderrahmane ETTOUNANI</h4>
+                <section>
+                    <div className='caroussel'></div>
+                    <div className='caroussel'></div>
+                    <div className='caroussel'></div>
+                    <div className='caroussel'></div>
+                </section>
             </div>
         );
     }
