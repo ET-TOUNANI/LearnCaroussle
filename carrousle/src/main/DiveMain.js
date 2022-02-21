720da4352e6ed7f7d59ad2e899d6b3d8102c08f5
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-21 12:44:48 -0800

    feat (body) : remove default code of React

---
+++ carrousle/src/main/DiveMain.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Divemain extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello from DivMain</h1>
+            </div>
+        );
+    }
+}
+
+export default Divemain;
