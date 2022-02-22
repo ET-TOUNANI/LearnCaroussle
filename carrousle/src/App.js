34537e9a99bf0d878b932ce07e459447ca8d44ab
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-22 14:35:32 -0800

    feat (divMain) : add count downer

--- carrousle/src/App.js
+++ carrousle/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import DiveMain from './main/DiveMain';
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
+      
         <DiveMain />
+
         </header>
       </div>
     );
--- carrousle/src/main/DiveMain.js
+++ carrousle/src/main/DiveMain.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
+
+import React,{ useState, useEffect } from 'react';
 import './divMain.css';
-class Divemain extends Component {
-    
-    render() {
-        const  allDiv=document.querySelectorAll('.caroussel')
-        let i=0
-        let j= allDiv.length-1
-        return (
-            <div>
-                <h4>What People say about Abderrahmane ETTOUNANI</h4>
+const Divemain = () => {
+
+    const  allDiv=document.querySelectorAll('.caroussel')
+    const [count, setCount] = useState(0);
+
+    // Similaire à componentDidMount et componentDidUpdate :
+    useEffect(() => {
+        // Met à jour le titre du document via l’API du navigateur
+        document.title = `Vous avez cliqué ${count} fois`;
+    });
+    return (
+        <div>
+            <h4>What People say about Abderrahmane ETTOUNANI</h4>
                 <section>
                     
                     {
-                        setInterval(() => {
-                            
-                            allDiv[i].setAttribute('className','first');
-                            allDiv[j].setAttribute('className','last');
-                            i++;
-                            j--;
-                        }, 2000)
+                        // setInterval(() => {
+                        //     console.log(allDiv)
+                        //     allDiv.forEach(div =>{
+                        //         div.style.left=div.style.left.value() - 10 + 'px';
+                        //     })
+                        //     // allDiv[i].setAttribute('className','first');
+                        //     // allDiv[j].setAttribute('className','las t');
+                        //     // i++;j--;
+                        // }, 2000)
                     }
                     
                     <div className='caroussel'></div>
                     <div className='caroussel'></div>
                     <div className='caroussel'></div>
                     <div className='caroussel'></div>
+                    <p>Vous avez cliqué {count} fois</p>
+                    <button onClick={() => setCount(count - 1)}>
+                        Cliquez ici
+                    </button>
                 </section>
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Divemain;
